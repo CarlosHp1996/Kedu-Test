8f5933c506fea5787ef1d63585ed1bf4d727d0eb
Carlos Santos <carloshpsantos1996@gmail.com>
2025-11-25 20:45:46 -0300

    Add project files.

---
+++ Kedu.Infra/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Kedu.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kedu.Infra.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                options.UseNpgsql(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
